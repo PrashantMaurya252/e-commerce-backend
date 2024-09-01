932a0770795382db75b3d31562b176721009c6ed
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-09-01 10:53:46 +0530

    getCurrent User and updateAccount Details controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -124,3 +124,30 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
     return res.status(200).json(new ApiResponse(200,{},"Password Change successfully"))
  })
 
+ const getCurrentUser = asyncHandler(async(req,res)=>{
+    return res.status(200).json(new ApiResponse(200,req?.user,"current user found successfully"))
+ })
+
+ const updateAccountDetails = asyncHandler(async(req,res)=>{
+    const {fullName,email} = req.body
+
+    if(!fullName || !email){
+        throw new ApiError(400,"All fields are required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req?.user._id,
+        {
+            $set:{
+                fullName,
+                email
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    return res.status(200).json(
+       new ApiResponse(200,user,"Account Details Updated Successfully")
+    )
+ })
+
