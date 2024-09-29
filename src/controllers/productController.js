a3d37e0b3ca8b8b9ad73289ec3b123e7152fa70a
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-09-29 22:17:06 +0530

    stripe payment route and controller modified

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -5,320 +5,343 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { Payment } from "../models/paymentModel.js";
 
+const addProduct = asyncHandler(async (req, res) => {
+  const { productName, productCategory, productPrice, productDescription } =
+    req.body;
 
-
-const addProduct = asyncHandler(async(req,res)=>{
-    const {productName,productCategory,productPrice,productDescription} = req.body
-
-    if([productName,productCategory,productPrice,productDescription].some((field)=>field.trim() === "")){
-        throw new ApiError(500,"All Fields are required")
-    }
-
-    const productImageLocalPath = req.file?.path
-
-    
-
-    if(!productImageLocalPath){
-        throw new ApiError(500,"product Image not found")
-    }
-
-    const productImage = await uploadOnCloudinary(productImageLocalPath)
-    if(!productImage){
-        throw new ApiError(400,"There is an error while product image uploading on cloudinary")
-    }
-    const product = await Product.create({
-        name:productName,
-        category:productCategory,
-        image:productImage.url,
-        description:productDescription,
-        price:productPrice
-    })
-
-    const createdProduct = await Product.findById(product._id)
-
-    if(!createdProduct){
-        throw new ApiError(500,"something went wrong while adding product")
-    }
-
-    return res.status(200).json(
-        new ApiResponse(200,createdProduct,"product added")
+  if (
+    [productName, productCategory, productPrice, productDescription].some(
+      (field) => field.trim() === ""
     )
-})
+  ) {
+    throw new ApiError(500, "All Fields are required");
+  }
 
-const markFavourite = asyncHandler(async(req,res)=>{
-    const userId = req.user._id
+  const productImageLocalPath = req.file?.path;
 
-    const productId = req.params.productId
+  if (!productImageLocalPath) {
+    throw new ApiError(500, "product Image not found");
+  }
 
-    const product = await Product.findById(productId)
-    if(!product){
-        throw new ApiError(500,"Product does not exist")
+  const productImage = await uploadOnCloudinary(productImageLocalPath);
+  if (!productImage) {
+    throw new ApiError(
+      400,
+      "There is an error while product image uploading on cloudinary"
+    );
+  }
+  const product = await Product.create({
+    name: productName,
+    category: productCategory,
+    image: productImage.url,
+    description: productDescription,
+    price: productPrice,
+  });
+
+  const createdProduct = await Product.findById(product._id);
+
+  if (!createdProduct) {
+    throw new ApiError(500, "something went wrong while adding product");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createdProduct, "product added"));
+});
+
+const markFavourite = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  const productId = req.params.productId;
+
+  const product = await Product.findById(productId);
+  if (!product) {
+    throw new ApiError(500, "Product does not exist");
+  }
+
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(400, "User does not exist");
+  }
+  const isFavourite = await user.favourites.some(
+    (fav) => fav.item.toString() === productId
+  );
+  if (isFavourite) {
+    throw new ApiError(400, "This Product already mark favourite");
+  }
+
+  user.favourites.push({
+    item: productId,
+    addedAt: Date.now(),
+  });
+
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user.favourites, "Product mark as favourite"));
+});
+
+const markUnFavourite = asyncHandler(async (req, res) => {
+  const productId = req.params.productId;
+  const userId = req.user._id;
+
+  const product = await Product.findById(productId);
+  if (!product) {
+    throw new ApiError(404, "Product does not exist");
+  }
+
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+  const isFavourite = await user.favourites.some(
+    (fav) => fav.item.toString() === productId
+  );
+
+  if (!isFavourite) {
+    throw new ApiError(400, "This product is not marked favourite");
+  }
+
+  user.favourites = user.favourites.filter(
+    (filter) => filter.item.toString() !== productId
+  );
+
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, user.favourites, "product unmarked successfully")
+    );
+});
+
+const getFavouriteProducts = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  const user = await User.findById(userId).populate({
+    path: "favourites.item",
+    model: "Product",
+    select: "name category image description price",
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const favouriteProducts = await user.favourites.map((fav) => fav.item);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        favouriteProducts,
+        "Favourite products request successful"
+      )
+    );
+});
+
+const getAllProduct = asyncHandler(async (req, res) => {
+  const userId = req?.user._id;
+
+  const user = await User.findById(userId).select("favourites");
+
+  // Adding Pagination
+
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  const price = req.query.price ? parseInt(req.query.price) : undefined;
+  const name = req.query.name ? req.query.name : undefined;
+
+  const filter = {};
+
+  if (price) {
+    filter.price = price;
+  }
+
+  if (name) {
+    filter.name = new RegExp(name, "i");
+  }
+
+  const aggregateQuery = Product.aggregate([
+    { $match: filter },
+    { $sort: { createdAt: -1 } },
+  ]);
+
+  const options = {
+    page,
+    limit,
+    customLabels: {
+      totalDocs: "totalItems",
+      docs: "products",
+    },
+  };
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const products = await Product.aggregatePaginate(aggregateQuery, options);
+
+  const favouriteProductIds = user.favourites.map((fav) => fav.item.toString());
+
+  products.products = products.products.map((product) => {
+    return {
+      ...product,
+      isFavourite: favouriteProductIds.includes(product._id.toString()),
+    };
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, products, "All Products are here"));
+});
+
+const addedToCart = asyncHandler(async (req, res) => {
+  const userId = req?.user._id;
+
+  const { productId, quantity } = req.body;
+
+  const product = await Product.findById(productId);
+
+  if (!product) {
+    throw new ApiError(404, "this product is not available");
+  }
+
+  const user = await User.findById(userId);
+
+  console.log(user, "user");
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  const productInCart = await user.cart.find(
+    (cartItem) => cartItem.item.toString() === productId
+  );
+  console.log(productInCart, "productInCart");
+
+  if (productInCart) {
+    productInCart.quantity += quantity || 1;
+  } else {
+    user.cart.push({
+      item: productId,
+      quantity: quantity || 1,
+    });
+  }
+
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user.cart, "item added to cart"));
+});
+
+const removeFromCart = asyncHandler(async (req, res) => {
+  const userId = req?.user._id;
+
+  const { productId, quantity, removeAll } = req.body;
+
+  const product = await Product.findById(productId);
+
+  if (!product) {
+    throw new ApiError(404, "this product is not available");
+  }
+
+  const user = await User.findById(userId);
+
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  const productInCart = await user.cart.find(
+    (cartItem) => cartItem.item.toString() === productId
+  );
+
+  if (!productInCart) {
+    throw new ApiError(404, "Product not found");
+  }
+
+  if (removeAll || productInCart.quantity <= quantity || quantity === 0) {
+    user.cart = user.cart.filter(
+      (filter) => filter.item.toString() !== productId
+    );
+  } else {
+    productInCart.quantity -= quantity || 1;
+
+    if (productInCart.quantity <= 0) {
+      user.cart = user.cart.filter(
+        (filter) => filter.item.toString() !== productId
+      );
     }
-
-    const user = await User.findById(userId)
-    if(!user){
-        throw new ApiError(400,"User does not exist")
-    }
-    const isFavourite = await user.favourites.some(fav => fav.item.toString() === productId)
-    if(isFavourite){
-        throw new ApiError(400,"This Product already mark favourite")
-    }
-
-    user.favourites.push({
-        item:productId,
-        addedAt:Date.now()
-    })
-
-    await user.save({validateBeforeSave:false})
-
-    return res.status(200).json(new ApiResponse(200,user.favourites,"Product mark as favourite"))
-})
-
-const markUnFavourite = asyncHandler(async(req,res)=>{
-    const productId = req.params.productId
-    const userId = req.user._id
-
-    const product = await Product.findById(productId)
-    if(!product){
-        throw new ApiError(404,"Product does not exist")
-    }
-
-    const user = await User.findById(userId)
-    if(!user){
-        throw new ApiError(404,"User does not exist")
-    }
-     const isFavourite = await user.favourites.some(fav => fav.item.toString() === productId)
-
-     if(!isFavourite){
-        throw new ApiError(400,"This product is not marked favourite")
-     }
-
-     user.favourites = user.favourites.filter(filter =>filter.item.toString() !== productId )
-
-     await user.save({validateBeforeSave:false})
-
-     return res.status(200).json(new ApiResponse(200,user.favourites,"product unmarked successfully"))
-})
-
-const getFavouriteProducts = asyncHandler(async(req,res)=>{
-    const userId = req.user._id
-
-    const user = await User.findById(userId).populate(
-        {
-            path:'favourites.item',
-            model:'Product',
-            select:'name category image description price'
-        }
-    )
-
-    if(!user){
-        throw new ApiError(404,"User not found")
-    }
-
-    const favouriteProducts = await user.favourites.map(fav => fav.item)
-    
-
-    return res.status(200).json(new ApiResponse(200,favouriteProducts,"Favourite products request successful"))
-})
-
-const getAllProduct = asyncHandler(async(req,res)=>{
-    const userId = req?.user._id
-
-    const user = await User.findById(userId).select('favourites')
-
-    // Adding Pagination
-
-    const page = parseInt(req.query.page) || 1
-    const limit = parseInt(req.query.limit) || 10
-
-    const price = req.query.price ? parseInt(req.query.price) : undefined
-    const name = req.query.name ? req.query.name : undefined
-
-    const filter ={}
-
-    if(price){
-        filter.price = price
-    }
-
-    if(name){
-        filter.name = new RegExp(name,'i')
-    }
-
-    const aggregateQuery = Product.aggregate([
-        {$match:filter},
-        {$sort:{createdAt:-1}}
-    ])
-
-    
-
-    const options = {
-        page,
-        limit,
-        customLabels:{
-            totalDocs:'totalItems',
-            docs:'products'
-        }
-    }
-
-
-
-    
-    if(!user){
-        throw new ApiError(404,"User not found")
-    }
-
-    const products = await Product.aggregatePaginate(aggregateQuery,options)
-
-    
-
-    const favouriteProductIds =user.favourites.map(fav => fav.item.toString())
-
-    
-
-     products.products= products.products.map((product)=>{
-        return {
-            ...product,
-            isFavourite:favouriteProductIds.includes(product._id.toString())
-        }
-    })
-
-    
-
-    return res.status(200).json(new ApiResponse(200,products,"All Products are here"))
-})
-
-const addedToCart = asyncHandler(async(req,res)=>{
-    
-    const userId = req?.user._id
-
-    const {productId,quantity} = req.body
-
-    const product = await Product.findById(productId)
-
-    if(!product){
-        throw new ApiError(404,"this product is not available")
-    }
-
-    const user = await User.findById(userId)
-
-    console.log(user,"user")
-
-    if(!user){
-        throw new ApiError(404,"User does not exist")
-    }
-
-    const productInCart = await user.cart.find(cartItem => cartItem.item.toString() === productId)
-    console.log(productInCart,"productInCart")
-
-    if(productInCart){
-        productInCart.quantity += quantity || 1
-    }else{
-        user.cart.push({
-            item:productId,
-            quantity:quantity || 1
-        })
-    }
-
-    await user.save({validateBeforeSave:false})
-
-    return res.status(200).json(new ApiResponse(200,user.cart,"item added to cart"))
-
-})
-
-const removeFromCart = asyncHandler(async(req,res)=>{
-    
-    const userId = req?.user._id
-
-    const {productId,quantity,removeAll} = req.body
-
-    const product = await Product.findById(productId)
-
-    if(!product){
-        throw new ApiError(404,"this product is not available")
-    }
-
-    const user = await User.findById(userId)
-
-    console.log(user,"user")
-
-    if(!user){
-        throw new ApiError(404,"User does not exist")
-    }
-
-    const productInCart = await user.cart.find(cartItem => cartItem.item.toString() === productId)
-
-    if(!productInCart){
-        throw new ApiError(404,"Product not found")
-    }
-
-    if(removeAll || productInCart.quantity <= quantity || quantity === 0){
-       user.cart = user.cart.filter(filter => filter.item.toString() !== productId)
-
-    }
-    
-    else{
-        productInCart.quantity -= quantity || 1
-
-        if(productInCart.quantity <=0){
-            user.cart = user.cart.filter(filter =>filter.item.toString() !==productId)
-        }
-    }
-
-    await user.save({validateBeforeSave:false})
-
-    return res.status(200).json(new ApiResponse(200,user.cart,"item removed to cart"))
-
-})
-
-const searchName =asyncHandler(async(req,res)=>{
-     const {searchTerm} = req.query
-
-     try {
-        const searchQuery = {$or:[
-            {name:{$regex:searchTerm,$options:'i'}},{category:{$regex:searchTerm,$options:'i'}}
-        ]}
-        
-
-        const product = await Product.find(searchQuery)
-
-
-        res.status(200).json(new ApiResponse(200,product,"Search api called successfully"))
-     } catch (error) {
-        throw new ApiError(404,"error in calling search api")
-     }
-})
-
-const stripePayment = asyncHandler(async(req,res)=>{
-    const {items} = req.body
-    console.log(items,"items")
+  }
+
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user.cart, "item removed to cart"));
+});
+
+const searchName = asyncHandler(async (req, res) => {
+  const { searchTerm } = req.query;
+
+  try {
+    const searchQuery = {
+      $or: [
+        { name: { $regex: searchTerm, $options: "i" } },
+        { category: { $regex: searchTerm, $options: "i" } },
+      ],
+    };
+
+    const product = await Product.find(searchQuery);
+
+    res
+      .status(200)
+      .json(new ApiResponse(200, product, "Search api called successfully"));
+  } catch (error) {
+    throw new ApiError(404, "error in calling search api");
+  }
+});
+
+const stripePayment = asyncHandler(async (req, res) => {
+    const {products,amount,userId} = req.body
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-    
+
     try {
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types:['card'],
-            line_items:items.map(item=>({
-                price_data:{
-                    currency:'inr',
-                    product_data:{
-                        name:item.name
-                    },
-                    unit_amount:item.price*100,
-                },
-                quantity:item.quantity
-            })),
-            mode:'payment',
-            success_url:'http://localhost:3000/success',
-            cancel_url:'http://localhost:3000/cancel'
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount:amount,
+            currency:'inr',
+            metaData:{userId,products:JSON.stringify(products)}
         })
-        console.log(session,"stripe session")
-        res.json({id:session.id})
 
-        
+        const payment = new Payment({
+            userId,
+            products,
+            amount,
+            paymentIntentId:paymentIntent.id,
+            status:'pending'
+        })
+        await payment.save({validateBeforeSave:false})
+        return res.status(200).json(200,{clientSecret:paymentIntent.client_secret})
     } catch (error) {
-        console.log(error.message,"stripe error")
-        throw new ApiError(500,"Something went wrong with payment api")
+        throw new ApiError(500,"Something went wrong with payment request")
     }
-})
+});
 
-export {addProduct,markFavourite,markUnFavourite,getFavouriteProducts,getAllProduct,addedToCart,removeFromCart,searchName,stripePayment}
+export {
+  addProduct,
+  markFavourite,
+  markUnFavourite,
+  getFavouriteProducts,
+  getAllProduct,
+  addedToCart,
+  removeFromCart,
+  searchName,
+  stripePayment,
+};
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -13,6 +13,7 @@ router.route('/all-products').get(verifyJWT,getAllProduct)
 router.route('/addedToCart').post(verifyJWT,addedToCart)
 router.route('/removeFromCart').post(verifyJWT,removeFromCart)
 router.route('/name-search').get(searchName)
+router.route('/create-payment-intent').post(stripePayment)
 
 
 
